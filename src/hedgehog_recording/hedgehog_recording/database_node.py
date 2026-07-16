f7ad200f9ac33e6cf4fd2350f09b3f0fff7f1ef4
Lukas Köppl <traktor.koeppl@gmail.com>
2026-07-16 13:22:47 +0200

    #9: refactor: move DB logic to SensorDB wrapper to clean up database_node

--- src/hedgehog_recording/hedgehog_recording/database_node.py
+++ src/hedgehog_recording/hedgehog_recording/database_node.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 from tdk_ussm_interfaces.msg import Envelope
 from .utils.database.database import Database
-from .utils.database.models import Measurement, SensorEnvelope
+from .utils.database.database_senoric import SensorDB
 
 
 class DatabaseNode(Node):
@@ -21,70 +21,38 @@ class DatabaseNode(Node):
             self.get_parameter("ussm_sensoren").get_parameter_value().string_value
         )
 
-        db_path = Path.cwd() / "output" / "databases" / db_name
-        self.db = Database(db_path)
-        self.db.init_db()
-
-        self.active_measurement_id: Optional[int] = None
-        self.sensors_received: Set[int] = set()
-
         start, end = map(int, sensor_range.split("-"))
-        self.sensor_map: Dict[str, int] = {
+        self._sensor_map: Dict[str, int] = {
             f"/ussm_envelope{i}": i for i in range(start, end + 1)
         }
 
-        for topic, s_id in self.sensor_map.items():
+        db_path = Path.cwd() / "output" / "databases" / db_name
+        self._db = Database(db_path)
+        self._sdb = SensorDB(list(self._sensor_map.values()), self._db)
+        self._db.init_db()
+
+        for topic, s_id in self._sensor_map.items():
             self.create_subscription(
-                Envelope, topic, self.get_callback_for_sensor(s_id), 10
+                Envelope, topic, self._get_callback_for_sensor(s_id), 10
             )
 
         self.get_logger().info(
-            f"Datenbank {db_name} bereit. Höre auf Sensoren: {list(self.sensor_map.values())}"
+            f"Datenbank {db_name} bereit. Höre auf Sensoren: {list(self._sensor_map.values())}"
         )
 
-    def get_callback_for_sensor(self, s_id: int) -> Callable[[Envelope], None]:
+    def _get_callback_for_sensor(self, s_id: int) -> Callable[[Envelope], None]:
         def callback(msg: Envelope) -> None:
-            self.envelope_callback(msg, s_id)
+            self._envelope_callback(msg, s_id)
 
         return callback
 
-    def get_or_create_measurement(self, sensor_id: int) -> int:
-        if sensor_id in self.sensors_received:
-            self.active_measurement_id = None
-            self.sensors_received.clear()
-
-        if self.active_measurement_id is None:
-            with self.db.session_scope() as session:
-                new_meas = Measurement(
-                    label="Auto-recorded", underground="unknown", conditions="unknown"
-                )
-                session.add(new_meas)
-                session.flush()
-                self.active_measurement_id = new_meas.id
-                self.get_logger().info(
-                    f"Neue Messung gestartet: ID {self.active_measurement_id}"
-                )
-
-        assert self.active_measurement_id is not None
-
-        self.sensors_received.add(sensor_id)
-        return self.active_measurement_id
-
-    def envelope_callback(self, msg: Envelope, s_id: int) -> None:
+    def _envelope_callback(self, msg: Envelope, s_id: int) -> None:
         try:
-            meas_id = self.get_or_create_measurement(s_id)
-
-            with self.db.session_scope() as session:
-                env = SensorEnvelope(
-                    measurement_id=meas_id,
-                    sensor_id=s_id,
-                    time_axis=list(msg.time_axis),
-                    amplitudes=list(msg.amplitudes),
-                )
-                session.add(env)
-
+            envelope_id, meas_id = self._sdb.insert_new_envelope(
+                s_id, list(msg.time_axis), list(msg.amplitudes)
+            )
             self.get_logger().info(
-                f"Gespeichert: Sensor {s_id} -> Messung ID {meas_id}"
+                f"Gespeichert: Sensor {s_id} -> Messung ID {meas_id}; Envelope ID:{envelope_id}"
             )
 
         except Exception as e:
---
+++ src/hedgehog_recording/hedgehog_recording/utils/database/database_senoric.py
@@ -0,0 +1,66 @@
+from typing import Dict, List
+from .database import Database
+from ..database.models import Measurement, SensorEnvelope
+
+
+class SensorDB:
+    def __init__(self, sensors: List[int], db: Database) -> None:
+        self._db = db
+
+        self._sensor_map: Dict[str, int] = {
+            f"/ussm_envelope{element}": element for element in sensors
+        }
+
+        self._received_sensor: Dict[int, bool] = {}
+        self._clear_recived_sensors()
+        self._active_measurement_id = None
+
+    def _clear_recived_sensors(self) -> None:
+        self._received_sensor: Dict[int, bool] = {
+            sensor_id: False for sensor_id in self._sensor_map.values()
+        }
+
+    def _check_sensor(self, new_sensor_id: int) -> bool:
+        return new_sensor_id in self._sensor_map
+
+    def _get_or_create_measurement(self, new_sensor_id: int) -> int:
+        all_sensors_received = False not in self._received_sensor.values()
+        sensor_already_received = self._received_sensor.get(new_sensor_id, False)
+
+        if all_sensors_received or sensor_already_received:
+            self._active_measurement_id = None
+            self._clear_recived_sensors()
+
+        if new_sensor_id in self._received_sensor:
+            self._received_sensor[new_sensor_id] = True
+
+        if self._active_measurement_id is None:
+            self._active_measurement_id = self.insert_new_measurement()
+
+        return self._active_measurement_id
+
+    def insert_new_measurement(self) -> int:
+        with self._db.session_scope() as session:
+            new_meas = Measurement(
+                label="Auto-recorded", underground="unknown", conditions="unknown"
+            )
+            session.add(new_meas)
+            session.flush()
+            self.active_measurement_id = new_meas.id
+        return self.active_measurement_id
+
+    def insert_new_envelope(self, sensor_id: int, time_axis: list, amplitudes: list):
+        meas_id = self._get_or_create_measurement(sensor_id)
+        envelope_id = None
+        with self._db.session_scope() as session:
+            envelope = SensorEnvelope(
+                measurement_id=meas_id,
+                sensor_id=sensor_id,
+                time_axis=time_axis,
+                amplitudes=amplitudes,
+            )
+            session.add(envelope)
+            session.flush()
+            envelope_id = envelope.id
+
+        return envelope_id, meas_id
